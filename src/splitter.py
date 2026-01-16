0c9d269ea705a8bcd904909d99f4e54494cf99b5
Dinesh B <bdinesh170601@gmail.com>
2026-01-16 09:08:38 +0530

    Add document splitting functionality

---
+++ src/splitter.py
@@ -0,0 +1,13 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def split_documents(documents):
+    """
+    Splits documents into smaller chunks.
+    """
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    chunks = text_splitter.split_documents(documents)
+    return chunks
